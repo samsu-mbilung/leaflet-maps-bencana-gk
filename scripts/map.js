51b748e01b6eb93a59e960da9f24bff46cf27103
samsu-mbilung <samsu-mbilung@users.noreply.github.com>
2021-12-27 11:43:23 +0700

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -900,26 +900,26 @@ $(window).on('load', function() {
   /**
    * Changes map attribution (author, GitHub repo, email etc.) in bottom-right
    */
-  function changeAttribution() {
-    var attributionHTML = $('.leaflet-control-attribution')[0].innerHTML;
-    var credit = 'View <a href="' + googleDocURL + '" target="_blank">data</a>';
-    var name = getSetting('_authorName');
-    var url = getSetting('_authorURL');
+ // function changeAttribution() {
+  //  var attributionHTML = $('.leaflet-control-attribution')[0].innerHTML;
+  //  var credit = 'View <a href="' + googleDocURL + '" target="_blank">data</a>';
+  //  var name = getSetting('_authorName');
+ //   var url = getSetting('_authorURL');
 
-    if (name && url) {
-      if (url.indexOf('@') > 0) { url = 'mailto:' + url; }
-      credit += ' by <a href="' + url + '">' + name + '</a> | ';
-    } else if (name) {
-      credit += ' by ' + name + ' | ';
-    } else {
-      credit += ' | ';
-    }
+ //   if (name && url) {
+ //     if (url.indexOf('@') > 0) { url = 'mailto:' + url; }
+ //     credit += ' by <a href="' + url + '">' + name + '</a> | ';
+//    } else if (name) {
+//      credit += ' by ' + name + ' | ';
+//    } else {
+ //     credit += ' | ';
+  //  }
 
-    credit += 'View <a href="' + getSetting('_githubRepo') + '">code</a>';
-    if (getSetting('_codeCredit')) credit += ' by ' + getSetting('_codeCredit');
-    credit += ' with ';
-    $('.leaflet-control-attribution')[0].innerHTML = credit + attributionHTML;
-  }
+//    credit += 'View <a href="' + getSetting('_githubRepo') + '">code</a>';
+//    if (getSetting('_codeCredit')) credit += ' by ' + getSetting('_codeCredit');
+//    credit += ' with ';
+//    $('.leaflet-control-attribution')[0].innerHTML = credit + attributionHTML;
+//  }
 
 
   /**
