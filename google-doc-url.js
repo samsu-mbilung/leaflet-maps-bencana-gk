b9220d8b8c494959c73d846adb07ca6ed23aa11d
samsu-mbilung <samsu-mbilung@users.noreply.github.com>
2021-12-29 09:39:25 +0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1BTk1OSS1vKrR1sc-9wl4KSnvehif8zAidmrIV5t7ESw/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyCBFojC6_NLobAdjYjPWZra-5xJMb5h1f8';
